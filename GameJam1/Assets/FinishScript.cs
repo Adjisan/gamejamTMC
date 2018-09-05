1bc8cd4a31fe5b069883d747078e470e302ad84c
Unknown <Robert.Rijkmans@hva.nl>
2018-09-05 12:40:15 +0200

    Added lava and finish triggers TODO: add following actions

---
+++ GameJam1/Assets/FinishScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishScript : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            StopAllCoroutines();
+            StartCoroutine(Restart());
+        }
+
+    }
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(2.5f); ///specify time needed
+        ///ToDo: add wat to next level
+    }
+}
